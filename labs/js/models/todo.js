b97c0ffd5ee82e1baf86fb9d5bdd9692495a5f85
Kyle Lieber <klieberuni@gmail.com>
2012-11-29 11:34:21 -0500

    lab 1: extend Backbone.Model and set defaults

--- labs/js/models/todo.js
+++ labs/js/models/todo.js
@@ -5,9 +5,13 @@ var app = app || {};
 
 	// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
 	// you'll want to extend the Backbone model here
-	
+	app.Todo = Backbone.Model.extend({
 		// Default attributes for the todo
 		// define the default attributes
+		defaults: {
+			title: '',
+			completed: false
+		},
 
 		// Toggle the `completed` state of this todo item.
 		toggle: function() {
